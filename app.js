c95bb0c458f6dd613feb460750d28c409a141a75
balakuthyaru <kgsdbalakrishna@gmail.com>
2024-04-21 19:57:43 -0400

    Defined error templates and validations

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
+const ExpressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
+const { campgroundSchema } = require('./schemas.js');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -15,58 +19,79 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs',ejsMate);
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded());
 app.use(methodOverride('_method'));
- 
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
 // app.get('/makecampground', async (req, res)=>{
 // const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
 // await camp.save();
 // res.send(camp);
 // })
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/campgrounds', async(req, res)=>{
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-})
-app.get('/campgrounds/new', (req, res)=>{
+    res.render('campgrounds/index', { campgrounds });
+}))
+app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
-app.post('/campgrounds', async(req, res)=>{
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
-app.get('/campgrounds/:id', async(req, res)=>{
+}))
+
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground });
-})
+    res.render('campgrounds/show', { campground });
+}))
 
-app.get('/campgrounds/:id/edit', async(req, res)=>{
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground });
-})
+    res.render('campgrounds/edit', { campground });
+}))
 
-app.put('/campgrounds/:id', async(req, res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`)});
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+}));
 
-app.delete('/campgrounds/:id', async(req, res)=>{
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id, {id});
-    res.redirect('/campgrounds')});
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id, { id });
+    res.redirect('/campgrounds')
+}));
 
 // app.get('/campgrounds', async(req, res)=>{
 //     const campgrounds = await Campground.find({});
 //     res.render('campgrounds/index', {campgrounds});
 // })
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))})
 
-app.listen(3000, () =>{
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
+app.listen(3000, () => {
     console.log('Serving 3000')
 })
