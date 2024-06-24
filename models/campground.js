13505054bf05a80cd5eaecd1d642302211ab7cb7
balakuthyaru <kgsdbalakrishna@gmail.com>
2024-06-24 19:14:19 -0400

    Fancy Cluster Maps

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
+const opts = { toJSON: { virtuals: true } };
+
 
 // https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
 
@@ -41,9 +43,13 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+},opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
-
-
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
